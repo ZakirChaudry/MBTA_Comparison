6e8208253b4482163f0dc20446edc5ea821040d2
Zakir Chaudry <zakirchaudry@gmail.com>
2019-07-15 21:14:26 -0400

    Created Plotly version (most likely will continue with this)

---
+++ Green_Line_Graph_Plotly.py
@@ -0,0 +1,95 @@
+# This file was created on July 15, 2019 by Zakir Chaudry
+import plotly
+import csv
+from typing import Dict, List
+
+
+def add_stop_to_dict(x: str, y: str, name: str, stop_line: str,
+                     stop_dict: Dict) -> None:
+    """Adds a stop to the given dictionary, given the coordinates, name, and
+    line"""
+    if stop_line not in stop_dict:
+        stop_dict[stop_line] = {}
+        stop_dict[stop_line]['x'] = [x]
+        stop_dict[stop_line]['y'] = [y]
+        stop_dict[stop_line]['name'] = [name]
+    else:
+        stop_dict[stop_line]['x'].append(x)
+        stop_dict[stop_line]['y'].append(y)
+        stop_dict[stop_line]['name'].append(name)
+
+
+if __name__ == '__main__':
+    green_line_csv = open('GreenLineStops.csv', 'r')
+    all_stops = list(csv.reader(green_line_csv))
+
+    stops_data = {}
+    for stop in all_stops:  # Converts CSV to Dictionary
+        stops_data[stop[2]] = {'x': float(stop[0]), 'y': 9000 - float(stop[1]),
+                               'next': stop[3], 'line': stop[4]}
+
+    stops = {}
+
+    for stop in stops_data:
+        stop_data = stops_data[stop]
+        if len(stop_data['line']) > 1:
+            lines = stop_data['line'].split(';')
+            for line in lines:
+                add_stop_to_dict(stop_data['x'], stop_data['y'],
+                                 stop, line, stops)
+        else:
+            add_stop_to_dict(stop_data['x'], stop_data['y'], stop,
+                             stop_data['line'], stops)
+
+    b_line = plotly.graph_objs.Scatter(
+        x=stops['B']['x'],
+        y=stops['B']['y'],
+        # To avoid duplicate labels
+        text=stops['B']['name'][:len(stops['B']['name']) - 4],
+        showlegend=False,
+        hoverinfo='text',
+        mode="markers+lines",
+        marker=dict(
+            color='green'
+        )
+    )
+
+    c_line = plotly.graph_objs.Scatter(
+        x=stops['C']['x'],
+        y=stops['C']['y'],
+        text=stops['C']['name'][:len(stops['C']['name']) - 4],
+        showlegend=False,
+        hoverinfo='text',
+        mode="markers+lines",
+        marker=dict(
+            color='green'
+        )
+    )
+
+    d_line = plotly.graph_objs.Scatter(
+        x=stops['D']['x'],
+        y=stops['D']['y'],
+        text=stops['D']['name'][:len(stops['D']['name']) - 2],
+        showlegend=False,
+        hoverinfo='text',
+        mode="markers+lines",
+        marker=dict(
+            color='green'
+        )
+    )
+
+    e_line = plotly.graph_objs.Scatter(
+        x=stops['E']['x'],
+        y=stops['E']['y'],
+        text=stops['E']['name'],
+        showlegend=False,
+        hoverinfo='text',
+        mode="markers+lines",
+        marker=dict(
+            color='green'
+        )
+    )
+
+    data = [b_line, c_line, d_line, e_line]
+
+    plotly.plotly.iplot(data, filename='basic-scatter')
